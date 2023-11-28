837452490dad0f26a42cba3f2742848726be9bfd
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-11-28 03:52:02 -0800

    Use body parser with express and add user route

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');  // Import expressjs
+const bodyParser = require('body-parser');  // Import body-parser
 const mongoose = require('mongoose');  // Import mongoose
 
 const port = process.env.PORT || 3000;
@@ -14,8 +15,13 @@ connection.once('open', () => {
 });
 
 // middleware
+app.use(bodyParser.urlencoded({extended: true}));
 app.use('/uploads', express.static('uploads'));  // Make uploads folder to make it accessible from browser
 app.use(express.json());  // For Json Data
+
+// User Route
+const userRoute = require('./routes/user');
+app.use('/user', userRoute); 
 
 app.route('/').get((req, res) => res.json('Hello World!'));
 // app.listen(port, () => console.log(`Your server is running on port ${port}`));
