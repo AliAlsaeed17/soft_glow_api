c000609639ce7d671edbc78319a9bab84eeefeda
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-12-05 02:28:25 -0800

    Print password

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -39,6 +39,7 @@ const loginUser = async (req, res) => {
 const registerUser = async (req, res) => {
   try {
     const { name, email, role, password} = req.body;
+    console.log(password);
     if (!name || !email || !password) {
         return res.status(400).json({ message: 'Name, email, and password are required' });
     }
